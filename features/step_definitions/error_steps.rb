3965c6d8e49b222aa72b552b01ed275712e249c7
Craig Anderson <craiga@craiga.id.au>
2011-04-20 15:46:57 +1000

    Detect a wider range of errors

--- features/step_definitions/error_steps.rb
+++ features/step_definitions/error_steps.rb
@@ -1,9 +1,13 @@
 Then /^(?:|I )should see no errors(?: within "([^"]*)")?$/ do |selector|
 	with_scope(selector) do
-	  if page.respond_to? :should
-	    page.should have_no_content('E_NOTICE')
-	  else
-	    assert page.has_no_content?('E_NOTICE')
-	  end
+		if page.respond_to? :should
+			page.should have_no_content('E_NOTICE')
+			page.should have_no_content('E_WARNING')
+			page.should have_no_content('SMTP Error')
+		else
+			assert page.has_no_content?('E_NOTICE')
+			assert page.has_no_content?('E_WARNING')
+			assert page.has_no_content?('SMTP Error')
+		end
 	end
 end
